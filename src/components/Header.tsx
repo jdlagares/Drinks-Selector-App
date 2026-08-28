5d9724600a9740fe82734e8c573208762d1d9491
Juan Diego Lagares Arrazola <sportymammal@gmail.com>
2026-08-28 10:55:24 -0600

    Create Header and navigation

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import {NavLink,useLocation} from "react-router-dom"
+
+export default function Header() {
+    const {pathname}=useLocation()
+  return (
+    <header className="bg-slate-800">
+        <div className="mx-auto container px-5 py-16">
+            <div className="flex justify-between items-center">
+                <div>
+                    <img className="w-32" src="/logo.svg" alt="logotipo" />
+                </div>
+                <nav className="flex gap-4">
+                    <NavLink className={({isActive})=> isActive? "text-orange-500 uppercase font-bold": "text-white uppercase font-bold" } to="/">Index</NavLink>
+                    <NavLink className={({isActive})=> isActive? "text-orange-500 uppercase font-bold": "text-white uppercase font-bold" } to="/favorites">Favorites</NavLink>
+                </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
---
+++ src/views/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+
+export default function FavoritesPage() {
+  return (
+    <>
+        <div>FavoritePage</div>
+    </>
+    
+  )
+}
