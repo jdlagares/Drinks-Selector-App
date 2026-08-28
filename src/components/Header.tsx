259e446f8616a468ebd18e2884c08bf124609660
Juan Diego Lagares Arrazola <sportymammal@gmail.com>
2026-08-28 14:40:49 -0500

    Implement Header and navlink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,39 @@
-import {NavLink,useLocation} from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
 export default function Header() {
-    const {pathname}=useLocation()
-  return (
-    <header className="bg-slate-800">
-        <div className="mx-auto container px-5 py-16">
-            <div className="flex justify-between items-center">
-                <div>
-                    <img className="w-32" src="/logo.svg" alt="logotipo" />
+    const { pathname } = useLocation()
+
+    const isHome = useMemo(() => pathname === "/", [pathname])
+    return (
+        <header className={isHome? "bg-header bg-center bg-cover":"bg-slate-800"}>
+            <div className="mx-auto container px-5 py-16">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <img className="w-32" src="/logo.svg" alt="logotipo" />
+                    </div>
+                    <nav className="flex gap-4">
+                        <NavLink className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"} to="/">Index</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"} to="/favorites">Favorites</NavLink>
+                    </nav>
                 </div>
-                <nav className="flex gap-4">
-                    <NavLink className={({isActive})=> isActive? "text-orange-500 uppercase font-bold": "text-white uppercase font-bold" } to="/">Index</NavLink>
-                    <NavLink className={({isActive})=> isActive? "text-orange-500 uppercase font-bold": "text-white uppercase font-bold" } to="/favorites">Favorites</NavLink>
-                </nav>
+                {isHome && (
+                    <form action="" className="md:w-1/2 2xl:w-1/3 bg-orange-400 mt-32 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4" >
+                            <label htmlFor="ingredient" className="block text-white uppercase font-extrabold text-lg">Name or Ingredients</label>
+                            <input id="ingredient" type="text" name="ingredient" className="p-3 w-full rounded-lg focus:outline-none bg-white" placeholder="Name or Ingredient. Ej Vodka, Tequila, Café" />
+                        </div>
+                        <div className="space-y-4">
+                            <label htmlFor="ingredient" className="block text-white uppercase font-extrabold text-lg">Categoria:</label>
+                            <select id="ingredient" name="ingredient" className="p-3 w-full rounded-lg focus:outline-none bg-white" >
+                                <option>---selection---</option>
+                            </select>
+                        </div>
+                        <input type="submit" value="Find Recipe" className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"/>
+
+                    </form>
+                )}
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
